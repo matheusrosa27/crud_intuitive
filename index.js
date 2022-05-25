583deeb0ab76f930ed209dee2340f85ab248ff87
Matheus Fernandes da Rosa <matheusmemphis@gmail.com>
2022-05-25 11:07:57 -0300

    connected to database

---
+++ index.js
@@ -0,0 +1,85 @@
+let express = require('express');
+let mysql = require('mysql');
+let app = express();
+let conexao = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'intuitive'
+});
+
+
+app.use(express.static('public'));
+conexao.connect(
+    (erro) => {
+        if (erro) {
+            console.log('Erro ao conectar:');
+            console.log(erro);
+        } else {
+            console.log('Conexão realizada com sucesso!');
+        }
+    }
+);
+
+app.get("/",(req,res) => {
+    res.sendFile(__dirname+"/public/index.html");
+});
+
+app.get("/public/functions.js",(req,res) => {
+    res.sendFile(__dirname+"/public/functions.js");
+});
+
+app.get("/consultarTabela",(req,res) => {
+    conexao.query("SELECT * FROM dados WHERE registro_ans > 0",
+    (erro,resultado) => {
+        if(erro) {
+            console.log(erro);
+            res.send("Deu erro");
+        } else {
+            res.send(resultado);
+        }
+    });
+});
+
+
+app.get("/adicionarCliente",(req,res) => {
+    let nome = req.query.nome;
+    let endereço = req.query.endereço;
+    let cep = req.query.cep;
+    let telefone = req.query.telefone;;
+    if (!nome || !endereço || !cep || !telefone){
+        res.status(400).send('Nome, endereço, CEP e telefone são obrigatórios!')
+    } else {
+        conexao.query("INSERT INTO clientes (NOME, ENDEREÇO, CEP, TELEFONE) VALUES(?,?,?,?)", [nome, endereço, cep, telefone],
+        (erro,resultado) => {
+            if(erro) {
+                console.log(erro);
+                res.status(500).send(erro);
+                res.send("Deu erro");
+            } else {
+                res.send(resultado);
+            }
+        });
+    }
+});
+
+app.get("/removerCliente",(req,res) => {
+    let id = req.query.id;
+
+    if (!id){
+        res.status(400).send('ID é obrigatório!');
+    } else {
+        conexao.query("DELETE FROM clientes WHERE id=?", [id],
+        (erro,resultado) => {
+            if(erro) {
+                console.log(erro);
+                res.status(500).send(erro);
+                res.send("Deu erro");
+            } else {
+                res.send(resultado);
+            }
+        });
+    }
+});
+
+app.listen(3000);
---
+++ script.sql
@@ -0,0 +1,36 @@
+DROP DATABASE IF EXISTS intuitive;
+CREATE DATABASE intuitive;
+USE intuitive;
+
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root';
+
+CREATE TABLE dados(
+	registro_ans int,
+    cnpj varchar(255), 
+    razao_social varchar(255),
+    nome_fantasia varchar(255),
+    modalidade varchar(255),
+    logradouro varchar(255),
+    log_numero varchar(255),
+    log_complemento varchar(255),
+    bairro varchar(255),
+    cidade varchar(255),
+    uf varchar(2),
+    cep int,
+    ddd int(2),
+    telefone int(9),
+    fax int,
+    email varchar(255),
+    representante varchar(255),
+    cargo_representante varchar(255),
+    data_registro_ans varchar(10)
+);
+
+LOAD DATA local INFILE 'C:\\Users\\Matheus\\intuitive\\Relatorio_cadop.csv' 
+INTO TABLE dados 
+FIELDS TERMINATED BY ';' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS;
+
+select * from dados;
